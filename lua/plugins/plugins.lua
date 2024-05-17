436983291201bcc35176c1e649a66f5d83af5edf
duhaesbaert <eduardo.haesbaert@gmail.com>
2024-05-16 22:10:19 -0300

    Add nvim configuration

---
+++ lua/plugins/plugins.lua
@@ -0,0 +1,10 @@
+return {
+  --{ "AlexvZyl/nordic.nvim" },
+  { "rose-pine/neovim" },
+  --{ "nanotech/jellybeans.vim" },
+  --{ "catppuccin/nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = { colorscheme = "rose-pine" },
+  },
+}
