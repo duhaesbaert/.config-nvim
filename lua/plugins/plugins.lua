6b67706b77b02f7b8d3f774d577a462c31d3437c
duhaesbaert <eduardo.haesbaert@gmail.com>
2024-05-17 21:37:43 -0300

    move colorschemes to separate file

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,10 +1,1 @@
-return {
-  --{ "AlexvZyl/nordic.nvim" },
-  { "rose-pine/neovim" },
-  --{ "nanotech/jellybeans.vim" },
-  --{ "catppuccin/nvim" },
-  {
-    "LazyVim/LazyVim",
-    opts = { colorscheme = "rose-pine" },
-  },
-}
+return {}
